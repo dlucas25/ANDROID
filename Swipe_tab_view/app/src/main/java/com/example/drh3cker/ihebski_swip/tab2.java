3371e55ad4ab186f94ce7af20fd43738f79e64ad
dlucas25 <daniel.mart259@gmail.com>
2015-11-19 20:23:49 -0600

    cambios

--- Swipe_tab_view/app/src/main/java/com/example/drh3cker/ihebski_swip/tab2.java
+++ Swipe_tab_view/app/src/main/java/com/example/drh3cker/ihebski_swip/tab2.java
@@ -3,6 +3,8 @@ package com.example.drh3cker.ihebski_swip;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +13,29 @@ import android.widget.Toast;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
+import java.util.ArrayList;
+
 /**
  * Created by Dr.h3cker on 14/03/2015.
  */
 public class tab2 extends Fragment {
-
+    private RecyclerView reciclador;
+    private RecyclerView.LayoutManager imanager;
+    private RecyclerView.Adapter adaptador;
     FloatingActionButton fab1,fab2,fab3;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 View view =inflater.inflate(R.layout.tab2,container,false);
+
+        ArrayList<News> newses = new ArrayList<News>();
+        newses.add(new News(R.mipmap.ic_launcher,"UPMP","BIS Universities"));
+        reciclador=(RecyclerView)view.findViewById(R.id.reciclador);
+        imanager=new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+        reciclador.setLayoutManager(imanager);
+        adaptador = new NewsAdapter(newses);
+        reciclador.setAdapter(adaptador);
+
         fab1 = (FloatingActionButton) view.findViewById(R.id.fab1);
         fab3 = (FloatingActionButton) view.findViewById(R.id.fab2);
         fab3 = (FloatingActionButton) view.findViewById(R.id.fab3);
