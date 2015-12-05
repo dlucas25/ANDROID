beac023bbbebbf2cde2df495386cbb639dd0cfeb
uriel <jesus.uriel.romero.lopez@gmail.com>
2015-12-05 00:06:49 -0600

    volley

--- Swipe_tab_view/app/src/main/java/com/example/drh3cker/ihebski_swip/tab1.java
+++ Swipe_tab_view/app/src/main/java/com/example/drh3cker/ihebski_swip/tab1.java
@@ -5,14 +5,23 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -63,6 +72,30 @@ public class tab1 extends Fragment
         return  view;
     }
 
+    public void onStart()
+    {
+        super.onStart();
+        String eventos = "http://django-upmp.rhcloud.com/noticias/api/noticias/";
+        // Create request queue
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity().getApplicationContext());
+        //  Create json array request
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.GET,eventos,new Response.Listener<JSONArray>()
+        {
+
+            public void onResponse(JSONArray jsonArray)
+            {
+
+                System.out.println(jsonArray);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("Error", "Unable to parse json array " + volleyError.toString());
+            }
+        });
+        // add json array request to the request queue
+        requestQueue.add(jsonArrayRequest);
+    }
 
     private View.OnClickListener clickListener = new View.OnClickListener(){
         public void onClick(View v){
